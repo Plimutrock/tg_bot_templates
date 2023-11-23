db3228c3f3b66bc38d2f88a0de536b2b7e978fac
plimutrock <dmorpheus@mail.ru>
2023-11-23 10:39:46 +0500

    Initial commit

---
+++ beginners_template/main.py
@@ -0,0 +1,30 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from config.config import Config, load_config
+from handlers import (user_handlers,
+                      other_handlers,
+                      admin_handlers)
+
+# Функция конфигурирования и запуска бота
+async def main() -> None:
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    # Инициализируем бот и диспетчер
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+
+    # Регистриуем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+    dp.include_router(admin_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
